878c4fb04c52f3cb3dae1e309e599d637f1113f8
Izhan Arif <iarif8369@jh-mba158-01.d214.org>
2018-01-09 08:47:18 -0600

    Made swift files for every view controller

---
+++ OfficialLibraryApp/writingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  writingViewController.swift
+//  OfficialLibraryApp
+//
+//  Created by Izhan Arif on 1/9/18.
+//  Copyright © 2018 Karolina Groszewska. All rights reserved.
+//
+
+import UIKit
+
+class writingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
