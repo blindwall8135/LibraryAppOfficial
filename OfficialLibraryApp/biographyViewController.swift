f7c083a2c0e98c5283808b85c56a4b8637660325
Izhan Arif <iarif8369@jh-mba158-01.d214.org>
2018-01-11 08:41:16 -0600

    Made biography code

--- OfficialLibraryApp/biographyViewController.swift
+++ OfficialLibraryApp/biographyViewController.swift
@@ -7,29 +7,51 @@
 //
 
 import UIKit
+import Firebase
 
 class biographyViewController: UIViewController {
 
+    var docRef: DocumentReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        docRef = Firestore.firestore().document("/BiographyInContext/0ReyRdGIexFqB2BbIihT")
+        docRef.getDocument { (MitchCourseWebsiteDocSnapshot, error) in
+            guard let MitchCourseWebsiteDocSnapshot = MitchCourseWebsiteDocSnapshot, MitchCourseWebsiteDocSnapshot.exists else { return }
+            let data = MitchCourseWebsiteDocSnapshot.data()
+            let MitchCourseWebsiteName = data["Name"] as? String ?? ""
+            let MitchCourseWebsiteLink = data["Link"] as? String ?? ""
+            print(MitchCourseWebsiteName)
+            print(MitchCourseWebsiteLink)
+        }
+        docRef = Firestore.firestore().document("/BiographyInContext/EmX2O5CkFdSzfTcyGCn3")
+        docRef.getDocument { (GeneticScienceLearningCenterDocSnapshot, error) in
+            guard let GeneticScienceLearningCenterDocSnapshot = GeneticScienceLearningCenterDocSnapshot, GeneticScienceLearningCenterDocSnapshot.exists else { return }
+            let data = GeneticScienceLearningCenterDocSnapshot.data()
+            let GeneticScienceLearningCenterName = data["Name"] as? String ?? ""
+            let GeneticScienceLearningCenterLink = data["Link"] as? String ?? ""
+            print(GeneticScienceLearningCenterName)
+            print(GeneticScienceLearningCenterLink)
+        }
+        docRef = Firestore.firestore().document("/BiographyInContext/LnsGoBujTRDJDb7HjbM6")
+        docRef.getDocument { (BiographyInContextCenterDocSnapshot, error) in
+            guard let BiographyInContextCenterDocSnapshot = BiographyInContextCenterDocSnapshot, BiographyInContextCenterDocSnapshot.exists else { return }
+            let data = BiographyInContextCenterDocSnapshot.data()
+            let BiographyInContextCenterName = data["Name"] as? String ?? ""
+            let BiographyInContextCenterLink = data["Link"] as? String ?? ""
+            print(BiographyInContextCenterName)
+            print(BiographyInContextCenterLink)
+        }
+        docRef = Firestore.firestore().document("/BiographyInContext/qOxMphl2cfaNRPrdiYl1")
+        docRef.getDocument { (HHMIBiointeractiveDocSnapshot, error) in
+            guard let HHMIBiointeractiveDocSnapshot = HHMIBiointeractiveDocSnapshot, HHMIBiointeractiveDocSnapshot.exists else { return }
+            let data = HHMIBiointeractiveDocSnapshot.data()
+            let HHMIBiointeractiveName = data["Name"] as? String ?? ""
+            let HHMIBiointeractiveLink = data["Link"] as? String ?? ""
+            print(HHMIBiointeractiveName)
+            print(HHMIBiointeractiveLink)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- OfficialLibraryApp/eBooksViewController.swift
+++ OfficialLibraryApp/eBooksViewController.swift
@@ -7,11 +7,87 @@
 //
 
 import UIKit
-
+import Firebase
 class eBooksViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+        var docRef : DocumentReference!
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            //E-Book
+            docRef = Firestore.firestore().document("/E-Book/XWFGC4GplLPmzpkPGLVH")
+            docRef.getDocument { (AmericansatWarDocSnapshot, error) in
+                guard let AmericansatWarDocSnapshot = AmericansatWarDocSnapshot, AmericansatWarDocSnapshot.exists else { return }
+                let data = AmericansatWarDocSnapshot.data()
+                let AmericansatWarName = data["Name"] as? String ?? ""
+                let AmericansatWarLink = data["Link"] as? String ?? ""
+                print(AmericansatWarName)
+                print(AmericansatWarLink)
+            }
+            docRef = Firestore.firestore().document("/E-Book/aEagJW7b9NcaLECI5icl")
+            docRef.getDocument { (CostumesandCultureDocSnapshot, error) in
+                guard let CostumesandCultureDocSnapshot = CostumesandCultureDocSnapshot, CostumesandCultureDocSnapshot.exists else { return }
+                let data = CostumesandCultureDocSnapshot.data()
+                let CostumesandCultureName = data["Name"] as? String ?? ""
+                let CostumesandCultureLink = data["Link"] as? String ?? ""
+                print(CostumesandCultureName)
+                print(CostumesandCultureLink)
+            }
+            docRef = Firestore.firestore().document("/E-Book/aG2xF2kAFdfUzyDDMAYR")
+            docRef.getDocument { (GenocideandCrimesAgainstHumanityDocSnapshot, error) in
+                guard let GenocideandCrimesAgainstHumanityDocSnapshot = GenocideandCrimesAgainstHumanityDocSnapshot, GenocideandCrimesAgainstHumanityDocSnapshot.exists else { return }
+                let data = GenocideandCrimesAgainstHumanityDocSnapshot.data()
+                let GenocideandCrimesAgainstHumanityName = data["Name"] as? String ?? ""
+                let GenocideandCrimesAgainstHumanityLink = data["Link"] as? String ?? ""
+                print(GenocideandCrimesAgainstHumanityName)
+                print(GenocideandCrimesAgainstHumanityLink)
+            }
+            docRef = Firestore.firestore().document("/E-Book/hboZZX5x6QvR6EL9f5yU")
+            docRef.getDocument { (BellBottomsDocSnapshot, error) in
+                guard let BellBottomsDocSnapshot = BellBottomsDocSnapshot, BellBottomsDocSnapshot.exists else { return }
+                let data = BellBottomsDocSnapshot.data()
+                let BellBottomsName = data["Name"] as? String ?? ""
+                let BellBottomsLink = data["Link"] as? String ?? ""
+                print(BellBottomsName)
+                print(BellBottomsLink)
+            }
+            docRef = Firestore.firestore().document("/E-Book/r3Eymfv2P4Eja7HCoWWB")
+            docRef.getDocument { (AmericanDecadesDocSnapshot, error) in
+                guard let AmericanDecadesDocSnapshot = AmericanDecadesDocSnapshot, AmericanDecadesDocSnapshot.exists else { return }
+                let data = AmericanDecadesDocSnapshot.data()
+                let AmericanDecadesName = data["Name"] as? String ?? ""
+                let AmericanDecadesLink = data["Link"] as? String ?? ""
+                print(AmericanDecadesName)
+                print(AmericanDecadesLink)
+            }
+            docRef = Firestore.firestore().document("/E-Book/x9sKSUQwf7DXx5FX9OEf")
+            docRef.getDocument { (CitiesofTheWorldDocSnapshot, error) in
+                guard let CitiesofTheWorldDocSnapshot = CitiesofTheWorldDocSnapshot, CitiesofTheWorldDocSnapshot.exists else { return }
+                let data = CitiesofTheWorldDocSnapshot.data()
+                let CitiesofTheWorldName = data["Name"] as? String ?? ""
+                let CitiesofTheWorldLink = data["Link"] as? String ?? ""
+                print(CitiesofTheWorldName)
+                print(CitiesofTheWorldLink)
+            }
+            docRef = Firestore.firestore().document("/E-Book/ydddtZ34liiFmXTtLaGN")
+            docRef.getDocument { (EarlyCivilizationsintheAmericasDocSnapshot, error) in
+                guard let EarlyCivilizationsintheAmericasDocSnapshot = EarlyCivilizationsintheAmericasDocSnapshot, EarlyCivilizationsintheAmericasDocSnapshot.exists else { return }
+                let data = EarlyCivilizationsintheAmericasDocSnapshot.data()
+                let EarlyCivilizationsintheAmericasName = data["Name"] as? String ?? ""
+                let EarlyCivilizationsintheAmericasLink = data["Link"] as? String ?? ""
+                print(EarlyCivilizationsintheAmericasName)
+                print(EarlyCivilizationsintheAmericasLink)
+            }
+            docRef = Firestore.firestore().document("/E-Book/zDZFkROi3BhEvpKVVHGo")
+            docRef.getDocument { (StJamesEncyclopediaofPopCultureDocSnapshot, error) in
+                guard let StJamesEncyclopediaofPopCultureDocSnapshot = StJamesEncyclopediaofPopCultureDocSnapshot, StJamesEncyclopediaofPopCultureDocSnapshot.exists else { return }
+                let data = StJamesEncyclopediaofPopCultureDocSnapshot.data()
+                let StJamesEncyclopediaofPopCultureName = data["Name"] as? String ?? ""
+                let StJamesEncyclopediaofPopCultureLink = data["Link"] as? String ?? ""
+                print(StJamesEncyclopediaofPopCultureName)
+                print(StJamesEncyclopediaofPopCultureLink)
+            }
 
         // Do any additional setup after loading the view.
     }
