1f20b7c9f947a247db5921b51cf7f2835958c6d1
Brian Lindwall <blindwall8135@jh-mba158-08.d214.org>
2018-01-25 08:18:02 -0600

    Ebooks

--- OfficialLibraryApp/eBooksViewController.swift
+++ OfficialLibraryApp/eBooksViewController.swift
@@ -8,10 +8,28 @@
 
 import UIKit
 import Firebase
+import SafariServices
 class eBooksViewController: UIViewController {
 
     
-        var docRef : DocumentReference!
+    @IBOutlet weak var AmericansatWarDocButton: UIButton!
+    @IBOutlet weak var CostumesandCultureButton: UIButton!
+    @IBOutlet weak var GenocideandCrimesAgainstHumanityButton: UIButton!
+    @IBOutlet weak var BellBottomsButton: UIButton!
+    @IBOutlet weak var AmericanDecadesButton: UIButton!
+    @IBOutlet weak var CitiesofTheWorldButton: UIButton!
+    @IBOutlet weak var EarlyCivilizationsintheAmericasButton: UIButton!
+    @IBOutlet weak var StJamesEncyclopediaofPopCultureButton: UIButton!
+    var link1 = String()
+    var link2 = String()
+    var link3 = String()
+    var link4 = String()
+    var link5 = String()
+    var link6 = String()
+    var link7 = String()
+    var link8 = String()
+
+    var docRef : DocumentReference!
         override func viewDidLoad() {
             super.viewDidLoad()
             
@@ -22,8 +40,9 @@ class eBooksViewController: UIViewController {
                 let data = AmericansatWarDocSnapshot.data()
                 let AmericansatWarName = data["Name"] as? String ?? ""
                 let AmericansatWarLink = data["Link"] as? String ?? ""
-                print(AmericansatWarName)
-                print(AmericansatWarLink)
+                self.AmericansatWarDocButton.setTitle(AmericansatWarName, for: .normal)
+                self.link1 = AmericansatWarLink
+
             }
             docRef = Firestore.firestore().document("/E-Book/aEagJW7b9NcaLECI5icl")
             docRef.getDocument { (CostumesandCultureDocSnapshot, error) in
@@ -31,8 +50,8 @@ class eBooksViewController: UIViewController {
                 let data = CostumesandCultureDocSnapshot.data()
                 let CostumesandCultureName = data["Name"] as? String ?? ""
                 let CostumesandCultureLink = data["Link"] as? String ?? ""
-                print(CostumesandCultureName)
-                print(CostumesandCultureLink)
+                self.CostumesandCultureButton.setTitle(CostumesandCultureName, for: .normal)
+                self.link2 = CostumesandCultureLink
             }
             docRef = Firestore.firestore().document("/E-Book/aG2xF2kAFdfUzyDDMAYR")
             docRef.getDocument { (GenocideandCrimesAgainstHumanityDocSnapshot, error) in
@@ -40,8 +59,8 @@ class eBooksViewController: UIViewController {
                 let data = GenocideandCrimesAgainstHumanityDocSnapshot.data()
                 let GenocideandCrimesAgainstHumanityName = data["Name"] as? String ?? ""
                 let GenocideandCrimesAgainstHumanityLink = data["Link"] as? String ?? ""
-                print(GenocideandCrimesAgainstHumanityName)
-                print(GenocideandCrimesAgainstHumanityLink)
+                self.GenocideandCrimesAgainstHumanityButton.setTitle(GenocideandCrimesAgainstHumanityName, for: .normal)
+                self.link3 = GenocideandCrimesAgainstHumanityLink
             }
             docRef = Firestore.firestore().document("/E-Book/hboZZX5x6QvR6EL9f5yU")
             docRef.getDocument { (BellBottomsDocSnapshot, error) in
@@ -49,8 +68,8 @@ class eBooksViewController: UIViewController {
                 let data = BellBottomsDocSnapshot.data()
                 let BellBottomsName = data["Name"] as? String ?? ""
                 let BellBottomsLink = data["Link"] as? String ?? ""
-                print(BellBottomsName)
-                print(BellBottomsLink)
+                self.BellBottomsButton.setTitle(BellBottomsName, for: .normal)
+                self.link4 = BellBottomsLink
             }
             docRef = Firestore.firestore().document("/E-Book/r3Eymfv2P4Eja7HCoWWB")
             docRef.getDocument { (AmericanDecadesDocSnapshot, error) in
@@ -58,8 +77,8 @@ class eBooksViewController: UIViewController {
                 let data = AmericanDecadesDocSnapshot.data()
                 let AmericanDecadesName = data["Name"] as? String ?? ""
                 let AmericanDecadesLink = data["Link"] as? String ?? ""
-                print(AmericanDecadesName)
-                print(AmericanDecadesLink)
+                self.AmericanDecadesButton.setTitle(AmericanDecadesName, for: .normal)
+                self.link5 = AmericanDecadesLink
             }
             docRef = Firestore.firestore().document("/E-Book/x9sKSUQwf7DXx5FX9OEf")
             docRef.getDocument { (CitiesofTheWorldDocSnapshot, error) in
@@ -67,8 +86,8 @@ class eBooksViewController: UIViewController {
                 let data = CitiesofTheWorldDocSnapshot.data()
                 let CitiesofTheWorldName = data["Name"] as? String ?? ""
                 let CitiesofTheWorldLink = data["Link"] as? String ?? ""
-                print(CitiesofTheWorldName)
-                print(CitiesofTheWorldLink)
+                self.CitiesofTheWorldButton.setTitle(CitiesofTheWorldName, for: .normal)
+                self.link6 = CitiesofTheWorldLink
             }
             docRef = Firestore.firestore().document("/E-Book/ydddtZ34liiFmXTtLaGN")
             docRef.getDocument { (EarlyCivilizationsintheAmericasDocSnapshot, error) in
@@ -76,8 +95,8 @@ class eBooksViewController: UIViewController {
                 let data = EarlyCivilizationsintheAmericasDocSnapshot.data()
                 let EarlyCivilizationsintheAmericasName = data["Name"] as? String ?? ""
                 let EarlyCivilizationsintheAmericasLink = data["Link"] as? String ?? ""
-                print(EarlyCivilizationsintheAmericasName)
-                print(EarlyCivilizationsintheAmericasLink)
+                self.EarlyCivilizationsintheAmericasButton.setTitle(EarlyCivilizationsintheAmericasName, for: .normal)
+                self.link7 = EarlyCivilizationsintheAmericasLink
             }
             docRef = Firestore.firestore().document("/E-Book/zDZFkROi3BhEvpKVVHGo")
             docRef.getDocument { (StJamesEncyclopediaofPopCultureDocSnapshot, error) in
@@ -85,27 +104,52 @@ class eBooksViewController: UIViewController {
                 let data = StJamesEncyclopediaofPopCultureDocSnapshot.data()
                 let StJamesEncyclopediaofPopCultureName = data["Name"] as? String ?? ""
                 let StJamesEncyclopediaofPopCultureLink = data["Link"] as? String ?? ""
-                print(StJamesEncyclopediaofPopCultureName)
-                print(StJamesEncyclopediaofPopCultureLink)
+                self.StJamesEncyclopediaofPopCultureButton.setTitle(StJamesEncyclopediaofPopCultureName, for: .normal)
+                self.link8 = StJamesEncyclopediaofPopCultureLink
             }
 
-        // Do any additional setup after loading the view.
     }
+    @IBAction func AmericansatWarDocPressed(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link1)! as URL)
+        self.present(svc, animated: true, completion: nil)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
+    @IBAction func CostumesandCulturePressed(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link2)! as URL)
+        self.present(svc, animated: true, completion: nil)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+    @IBAction func GenocideandCrimesAgainstHumanityPressed(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link3)! as URL)
+        self.present(svc, animated: true, completion: nil)
 
+    }
+    @IBAction func BellBottomsPressed(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link4)! as URL)
+        self.present(svc, animated: true, completion: nil)
+
+    }
+    @IBAction func AmericanDecadesPressed(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link5)! as URL)
+        self.present(svc, animated: true, completion: nil)
+
+    }
+    @IBAction func CitiesofTheWorldPressed(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link6)! as URL)
+        self.present(svc, animated: true, completion: nil)
+
+    }
+    @IBAction func EarlyCivilizationsintheAmericasPressed(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link7)! as URL)
+        self.present(svc, animated: true, completion: nil)
+
+    }
+    @IBAction func StJamesEncyclopediaofPopCulturePressed(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link8)! as URL)
+        self.present(svc, animated: true, completion: nil)
+
+    }
 }
+
+
+
