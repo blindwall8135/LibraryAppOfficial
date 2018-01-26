2b06ca45d8ea165d8867e7ff84405aec2ef30bd1
Brian Lindwall <blindwall8135@jh-mba158-08.d214.org>
2018-01-26 08:38:03 -0600

    newspaper

--- OfficialLibraryApp/databaseNewspapersViewController.swift
+++ OfficialLibraryApp/databaseNewspapersViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 import Firebase
+import SafariServices
+
 
 class databaseNewspapersViewController: UIViewController {
-    
+    var link1 =  String()
+    var link2 =  String()
+    var link3 =  String()
+    var link4 =  String()
     @IBOutlet weak var newspaperSourcePlus: UIButton!
     @IBOutlet weak var newspaperSource: UIButton!
     @IBOutlet weak var aPNews: UIButton!
     @IBOutlet weak var webNews: UIButton!
+    
     var docRef : DocumentReference!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +30,8 @@ class databaseNewspapersViewController: UIViewController {
             let data = newspaperSourcePlusDocSnapshot.data()
             let newspaperSourcePlusName = data["Name"] as? String ?? ""
             let newspaperSourcePlusLink = data["Link"] as? String ?? ""
-            print(newspaperSourcePlusName)
-            print(newspaperSourcePlusLink)
+            self.newspaperSourcePlus.setTitle(newspaperSourcePlusName, for: .normal)
+            self.link1 = newspaperSourcePlusLink
         }
         docRef = Firestore.firestore().document("/Database Newspapers/Dyfmrqa27CSYBTbgntge")
         docRef.getDocument { (aPNewsDocSnapshot, error) in
@@ -33,8 +39,8 @@ class databaseNewspapersViewController: UIViewController {
             let data = aPNewsDocSnapshot.data()
             let aPNewsName = data["Name"] as? String ?? ""
             let aPNewsLink = data["Link"] as? String ?? ""
-            print(aPNewsName)
-            print(aPNewsLink)
+            self.aPNews.setTitle(aPNewsName, for: .normal)
+            self.link2 = aPNewsLink
         }
         docRef = Firestore.firestore().document("/Database Newspapers/DzdwJG5SaKHKaOjtORfm")
         docRef.getDocument { (newspaperSourceDocSnapshot, error) in
@@ -42,8 +48,8 @@ class databaseNewspapersViewController: UIViewController {
             let data = newspaperSourceDocSnapshot.data()
             let newspaperSourceName = data["Name"] as? String ?? ""
             let newspaperSourceLink = data["Link"] as? String ?? ""
-            print(newspaperSourceName)
-            print(newspaperSourceLink)
+            self.newspaperSource.setTitle(newspaperSourceName, for: .normal)
+            self.link3 = newspaperSourceLink
         }
         docRef = Firestore.firestore().document("/Database Newspapers/ob6HlcVj5QUhcghwoCq8")
         docRef.getDocument { (webNewsDocSnapshot, error) in
@@ -51,17 +57,25 @@ class databaseNewspapersViewController: UIViewController {
             let data = webNewsDocSnapshot.data()
             let webNewsName = data["Name"] as? String ?? ""
             let webNewsLink = data["Link"] as? String ?? ""
-            print(webNewsName)
-            print(webNewsLink)
+            self.webNews.setTitle(webNewsName, for: .normal)
+            self.link4 = webNewsLink
         }
     }
     @IBAction func newspaperSourcePlus(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link1)! as URL)
+        self.present(svc, animated: true, completion: nil)
     }
     
     @IBAction func aPNews(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link2)! as URL)
+        self.present(svc, animated: true, completion: nil)
     }
     @IBAction func newspaperSource(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link3)! as URL)
+        self.present(svc, animated: true, completion: nil)
     }
     @IBAction func webNews(_ sender: Any) {
+        let svc = SFSafariViewController(url: NSURL(string: link4)! as URL)
+        self.present(svc, animated: true, completion: nil)
     }
 }
